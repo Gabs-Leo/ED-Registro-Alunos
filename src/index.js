da836d9b8b9e7daa6d5002b2c8fafe6aed150cea
Gabs-Leo <gabrieldemoraistaveira@gmail.com>
2023-09-11 09:05:36 -0300

    Added container update method

--- src/index.js
+++ src/index.js
@@ -10,17 +10,39 @@ class Aluno{
 }
 
 class AlunoService {
+    constructor(containerId) {
+        this.containerId = containerId;
+        this.updateContainer();
+    }
+    updateContainer = () => {
+        const alunos = getAlunosFromStorage();
+        let result = "";
+        alunos.forEach((aluno) => {
+            result +=`
+            <tr>
+                <td>${aluno.nome}</td>
+                <td>${aluno.ra}</td>
+                <td>${aluno.idade}</td>
+                <td>${aluno.sexo}</td>
+                <td>${aluno.media}</td>
+                <td>${aluno.resultado}</td>
+            </tr>
+            `
+        })
+        document.querySelector(this.containerId).innerHTML = result;
+    }
     getAlunosFromStorage = () => {
         const alunos = localStorage.getItem("alunos");
         if(alunos)
             return JSON.parse(alunos);
         localStorage.setItem("alunos", JSON.stringify([]))
         return [];
-        
     }
     save = (aluno) => {
         const alunos = getAlunosFromStorage();
         alunos.push(aluno);
+        localStorage.setItem("alunos", JSON.stringify(alunos))
+        this.updateContainer();
     }
     //Valid Sort Types: name, ra, approved 
     listBySortType = (sortType) => {
@@ -30,10 +52,11 @@ class AlunoService {
             case "approved":
                 break;
             default:
-                ;
+                break;
         }
     }
     clearData = () => {
-        
+        localStorage.setItem("alunos", JSON.stringify([]));
+        this.updateContainer();
     }
 }
